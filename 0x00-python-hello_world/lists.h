464f6307ac25ee78e7636ac4d8d0a8d7b170c07d
Alexander Iwuchukwu <iwuchukwualexander@gmail.com>
2023-08-10 21:32:58 -0700

    Updated files

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -6,19 +6,21 @@
 /**
  * struct listint_s - singly linked list
  * @n: int
- * @next: pointing to the next node
- * Description: This is a singly linked list node structure
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listin(const listint_t *h);
+size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
+
