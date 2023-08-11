6f49c0873c9128fcfae1097b0ddc18dd583e3daa
Alexander Iwuchukwu <iwuchukwualexander@gmail.com>
2023-08-10 21:21:54 -0700

    Updated files

---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,16 @@
+ALX SE COHORT 16
+
+0x00. Python - Hello, World
+
+Tasks
+
+0. Run Python file - Write a Shell script that runs a Python script.
+1. Run inline - Write a Shell script that runs Python code
+2. Hello, print -
+3. Print integer
+4. Print float
+5. Print string
+6. Play with strings
+7. Copy - Cut - Paste
+8. Create a new sentence
+9. Easter Egg
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: int
+ * @next: pointing to the next node
+ * Description: This is a singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listin(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif
