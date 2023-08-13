056d73a3f9be5c94942d3e259ce7172e9797dc81
Alexander Iwuchukwu <iwuchukwualexander@gmail.com>
2023-08-13 16:07:06 -0700

    Updated files

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 def element_at(my_list, idx):
     """Retrieves an element from a list like in C"""
     if idx < 0:
-        return('None')
+        return None
     elif idx > (len(my_list)-1):
         return None
     return (my_list[idx])
