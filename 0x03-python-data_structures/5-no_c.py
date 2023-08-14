b39d4932e0712ad2d44c6807ec6bcec67601209e
Alexander Iwuchukwu <iwuchukwualexander@gmail.com>
2023-08-14 09:38:58 -0700

    Updated files

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 def no_c(my_string):
     for i in my_string:
-        if i = 'c' or i = 'C':
-            copy = my_string.remove(i)
+        if my_string[i] = 'c' or my_string[i] = 'C':
+            copy = my_string.del(i)
         return (my_string)
     return (copy)
